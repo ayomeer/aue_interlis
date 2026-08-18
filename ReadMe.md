3d1f9ea58017731257f7142e36389291a90cc0ec
Andreas <ayomeer@gmail.com>
2026-08-18 15:33:36 +0200

    Formatting and argparse argument help strings.

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# aue_interlis
+
+Centralized repo for all things schema modelling.
+
+## Todo
+
+- [x] Implement argparse for  `utility/render_sql_template.py` 
+  - path_rendering_args
+  - path_out
--- utility/post_sql_script_generation/render_sql_template.py
+++ utility/post_sql_script_generation/render_sql_template.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
@@ -6,11 +7,9 @@ from jinja2 import Environment, FileSystemLoader
 # Constants
 PROJECT_ROOT = Path(__file__).resolve().parent
 
-
 # Paths
 path_templates = Path(PROJECT_ROOT) / "templates"
-path_json_args = Path(PROJECT_ROOT) / "rendering_args.json"
-path_out = Path(PROJECT_ROOT) / "output"
+default_output_path = Path(PROJECT_ROOT) / "output"
 
 # Jinja Rendering Setup
 env = Environment(
@@ -21,12 +20,42 @@ env = Environment(
 
 if __name__ == '__main__':
 
-  # Get rendering args
-  with open(path_json_args) as json_file:
+  # -- Parse Input --------------------------------------------------------------------------------
+  parser = argparse.ArgumentParser(
+    description="""
+        Utility for generating SQL scripts for common post INTERLIS schemaimport DDL changes and 
+        additions. Takes a rendering args as json and outputs SQL Script at given output path.""",
+    add_help=True,
+  )
+  parser.add_argument(
+      "--path_rendering_args",
+      "-a",
+      dest="path_rendering_args",
+      type=Path,
+      required=True,
+      help="Path to rendering args json file.",
+  )
+  parser.add_argument(
+      "--path_output",
+      "-o",
+      dest="path_output",
+      type=Path,
+      default=default_output_path, # required=True,
+      help="Path of output directory for generated file.",
+  )
+  args = parser.parse_args()
+
+  # -- Render SQL Script from Template ------------------------------------------------------------
+  print(f"Loading file {args.path_rendering_args.resolve()}.")
+  with open(args.path_rendering_args) as json_file:
     rendering_args = json.load(json_file)
 
   # Render templates
   template = env.get_template("t_post_import.sql.j2")
   sql = template.render(**rendering_args)
-  output_file = Path(path_out) / f"post_sql_{rendering_args["target_schema"]}.sql"
+
+  # Write text to file
+  output_file = Path(args.path_output) / f"post_sql_{rendering_args["target_schema"]}.sql"
+
+  print(f"Writing output to {output_file.resolve()}")
   output_file.write_text(sql)
